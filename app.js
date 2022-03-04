2661b282fd7068bc21e9ada72811433da38f53d5
Yanik Nayee <yaniknayee123@gmail.com>
2022-03-04 09:57:51 -0500

    added skeleton and UI, Book class and methods to make book show on the site

--- app.js
+++ app.js
@@ -1,31 +1,98 @@
 
-const title = document.getElementById('bookTitle')
-const author = document.getElementById('bookAuthor')
-const pages = document.getElementById('bookPages')
-const form = document.getElementById('form')
 
-
-
-let myLibrary = [];
-
-function Book(title, author, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
-    this.sayInfo = function() {
-        info = String(`${title} by ${author}, ${pages}, ${status}`)
-        return info
+//Book class and constructor
+class Book{
+    constructor(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
     }
 }
 
+//User Interface methods
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Harry Potter',
+                author: 'J.K.Rowling',
+                pages: '246'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+//add books to list
+    static addBookToList(book){
+        const List = document.getElementById('book-list');
+        const row = document.createElement('tr');
+//create row in table with each books information in each
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete"> X </td>
+        `;
+
+        List.appendChild(row)
+    }
+
+    static clearFields() {
+        document.querySelector('#bookTitle').value = '';
+        document.querySelector('#bookAuthor').value = '';
+        document.querySelector('#bookPages').value = '';
+    }
+}
+
+//display books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+//add a book
+document.querySelector('#form').addEventListener('submit', (e) =>
+ {  
+
+    //prevent submitting
+    e.preventDefault();
+
+     //values from the form
+     const title = document.querySelector('#bookTitle').value;
+     const author = document.querySelector('#bookAuthor').value;
+     const pages = document.querySelector('#bookPages').value;
+     
+
+     //instantiate book
+     const book = new Book(title, author, pages);
+
+    //add book to ui
+    UI.addBookToList(book);
+
+    //clear input fields
+    UI.clearFields();
+ }
+);
+
+//remove a book from list
+document.querySelector('#book-list').addEventListener('click' , (e) =>
+    {
+        console.log(e.target)
+    }
+)
+
+
+
+
+
+
+/*
 function addBookToLibrary() {
     form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        checkInputs();
-        $(e).unbind('submit').submit()
+        myLibrary.push(new Book(title, author, pages, status))
+
     })
-    const TheHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read')
+    
 }
 
 addBookToLibrary();
+*/
